184b94f6f035218535b0ea3511d7294c2e557f24
Mark Tiedemann <www.marktiedemann@gmail.com>
2022-05-01 14:09:08 +0200

    Misc changes

--- wscript.test.js
+++ wscript.test.js
@@ -35,10 +35,11 @@ WScript.StdOut.WriteBlankLines(2);
 var stdIn = WScript.StdIn;
 WScript.Echo(stdIn.Column); // 1
 WScript.Echo(stdIn.Line); // 1
+if (typeof stdIn.Line === "number") {
+	WScript.Echo("Stream open");
+}
 stdIn.Close();
-try {
-	stdIn.AtEndOfStream;
-} catch (_) {
+if (typeof stdIn.Line === "unknown") {
 	WScript.Echo("Stream closed");
 }
 
